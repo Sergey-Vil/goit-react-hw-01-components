292a597c2a685a45397dc9ed1d4bb4d6c6a64f54
Sergey-Vil <vilchinskiy.s.st@gmail.com>
2023-01-31 17:54:34 +0200

    task-01
    task-01

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import {Profile}  from './Profile/Profile'
+import  user   from '../assets/user.json'
+
 export const App = () => {
   return (
     <div
@@ -10,8 +13,15 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
-      Hello World
+    
+      <Profile
+        username={user.username}
+        
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+/> 
     </div>
   );
 };
